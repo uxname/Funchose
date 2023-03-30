59e41876f88991abb78b388e9fa1b9c0e697b50e
uxname <uxname@gmail.com>
2023-03-30 13:38:53 +0300

    Release

---
+++ dist/src/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Detector = void 0;
+var detector_1 = require("./detector");
+Object.defineProperty(exports, "Detector", { enumerable: true, get: function () { return detector_1.Detector; } });
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,1 @@
+export { Detector } from './detector';
