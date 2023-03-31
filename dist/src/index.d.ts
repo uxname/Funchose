96bdb2e609ce9e314dded64dd9226e54f3071b50
uxname <uxname@gmail.com>
2023-03-31 16:25:26 +0300

    Export checkers

--- dist/src/index.d.ts
+++ dist/src/index.d.ts
@@ -1,1 +1,4 @@
+export { BaseChecker } from './checkers/base/base.checker';
+export { EvmPrivateKeyChecker } from './checkers/evm-private-key/evm-private-key.checker';
+export { PasswordChecker } from './checkers/password/password.checker';
 export { Detector } from './detector';
